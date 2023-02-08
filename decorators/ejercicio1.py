7490486782d4e8ab7ce9ea66e52252573d639bfb
Pedro Lopez <>
2023-02-08 09:11:48 +0100

    finish exercise 1

--- decorators/ejercicio1.py
+++ decorators/ejercicio1.py
@@ -1,28 +1,60 @@
 # Escribe un decorador que cambie el resultado de la función decorada a mayúsculas
+import functools
 from typing import Callable
 
 
-def to_uppercase(func: Callable) -> Callable:
-    print(f'{"Entramos en el decorador"}')
+def conditional_uppercase(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> str:
+        original_value = func(*args, **kwargs)
+        lenght = len(original_value)
 
-    def wrapper() -> str:
-        original_value = func()
-        return original_value.upper()
-
-    print(f'{"salimos del decorador"}')
+        if lenght % 2 == 0:
+            return func(*args, **kwargs), lenght
+        else:
+            return original_value.upper(), lenght
 
     return wrapper
 
 
-@to_uppercase
-def first_test_func() -> str:
-    return f'{"Esto es la primera string de retorno"}'
+@conditional_uppercase
+def test_func_even() -> str:
+    return f'{"Esto es una string par"}'
 
 
-@to_uppercase
-def second_test_func() -> str:
-    return f'{"Esto es la segunda string de retorno"}'
+@conditional_uppercase
+def test_func_odd() -> str:
+    return f'{"Esto es otra string impar"}'
 
 
-print(first_test_func())
-print(second_test_func())
+@conditional_uppercase
+def test_func_one_arg(param: str) -> str:
+    return f"Esto es una string con un parametro  {param}"
+
+
+@conditional_uppercase
+def test_func_two_arg(param: str, another_param: str) -> str:
+    return f"Esto es una string con dos param {param}, {another_param}"
+
+
+print(test_func_even)
+return_value, num_char = test_func_even()
+print(return_value, num_char)
+
+print("\n")
+
+print(test_func_odd)
+return_value, num_char = test_func_odd()
+print(return_value, num_char)
+
+print("\n")
+
+print(test_func_one_arg)
+return_value, num_char = test_func_one_arg("par")
+print(return_value, num_char)
+
+print("\n")
+
+print(test_func_two_arg)
+return_value, num_char = test_func_two_arg("par", "imp")
+print(return_value, num_char)
