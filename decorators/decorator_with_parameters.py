b63cf7ab08163c7169a204de29de7f347cd6f1e2
Pedro lopez <plopezpe@redhat.com>
2023-02-09 21:20:20 +0100

    decoratorwithparam

--- decorators/decorator_with_parameters.py
+++ decorators/decorator_with_parameters.py
@@ -4,14 +4,13 @@ from typing import Tuple
 
 
 def decorator_factory(input_arg: int) -> Callable:
-
     def conditional_uppercase(func: Callable) -> Callable:
         @functools.wraps(func)
-        def conditional_uppercase_wrapper(*args, **kwargs) -> Tuple[str,int]:
+        def conditional_uppercase_wrapper(*args, **kwargs) -> Tuple[str, int]:
             original_value = func(*args, **kwargs)
             lenght = len(original_value)
-            
-            if input_arg %2 ==0: 
+
+            if input_arg % 2 == 0:
                 return original_value, lenght
             else:
                 return original_value.upper(), lenght
@@ -20,17 +19,21 @@ def decorator_factory(input_arg: int) -> Callable:
 
     return conditional_uppercase
 
+
 @decorator_factory(2)
 def test_return_even_str() -> str:
     return f'{"this is even"}'
+
 
 @decorator_factory(1)
 def test_return_odd_str() -> str:
     return f'{"this is odd"}'
 
+
 @decorator_factory(1)
 def test_arg_to_str(param: str) -> str:
     return f"the arg {param}"
+
 
 first_return = test_return_even_str()
 print(first_return)
